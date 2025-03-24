63f7e80906ba0affa511316598317a3dfea23f03
Sveinbjörn Geirsson <sveinbjorn@sveinbjorn.dev>
2025-03-24 12:34:16 +0000

    Update auth.js

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,6 +4,7 @@ const authController = require('../controllers/authController');
 
 router.get('/login', authController.login);
 router.get('/callback', authController.callback);
+router.post('/token', authController.token); // Add this line
 router.post('/refresh', authController.refresh);
 router.get('/auth_callback', authController.auth_callback);
 
